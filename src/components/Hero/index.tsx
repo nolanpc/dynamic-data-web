1bdbdddcb98ca7c0c301529eaed431ef846f97a6
Nolan Vasquez <nvasquez_darient@mercantilbanco.com.pa>
2026-05-11 17:56:12 -0400

    change route

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -20,13 +20,13 @@ const Hero = () => {
                 </p>
                 <div className="flex flex-col items-center text-left space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
-                    href="http://localhost:3000/contact"
+                    href="/contact"
                     className="rounded-xs bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
                     🗣️ Inicia una consulta
                   </Link>
                   <Link
-                    href="http://localhost:3000/contact"
+                    href="/contact"
                     className="inline-block rounded-xs bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                   >
                     Cómo trabajamos
